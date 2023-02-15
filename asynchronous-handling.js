f3ba54fa3125c268aede2821a9f03ca33b0c65e8
Nishimwe Prince <princeelysee@gmail.com>
2023-02-15 21:47:42 +0200

    Added async javascript

---
+++ asynchronous-handling.js
@@ -0,0 +1,46 @@
+let families = [  
+    {fatherName: "Izere", motherName: "Cici", childrenNumber: 4},
+    {fatherName: "Ntare", motherName: "Kelly", childrenNumber: 1},
+    {fatherName: "Fabrice", motherName: "Chacha", childrenNumber: 3},
+    {fatherName: "Eric", motherName: "Ange", childrenNumber: -1},
+    {fatherName: "Derick", motherName: "Belle", childrenNumber: 0}
+  ];
+  
+  let calculateFamilyRatios = (families) => {
+    let totalChildren = 0;
+    let eligibleFamilies = [];
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        for (let i = 0; i < families.length; i++) {
+          if (families[i].childrenNumber < 0) {
+            console.log("Skipping family with negative number of children.");
+            continue;
+          }
+  
+          if (families[i].fatherName.toLowerCase() === "yves") {
+            return reject("Yves is not an allowed dad in 2022.");
+          }
+  
+          totalChildren += families[i].childrenNumber;
+        }
+  
+        for (let i = 0; i < families.length; i++) {
+          if (families[i].childrenNumber > 0) {
+            families[i].newMember = totalChildren / families.length;
+            eligibleFamilies.push(families[i]);
+          }
+        }
+  
+        if (eligibleFamilies.length > 0) {
+          resolve(eligibleFamilies);
+        } else {
+          reject("No families with children found.");
+        }
+      }, 1000);
+    });
+  };
+  
+  calculateFamilyRatios(families)
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
